29d68c734100f43e2af3f46da8b59c9a5b6484d3
Hokai-Hokai <grn.pa06@gmail.com>
2022-07-28 00:12:42 +0200

    front + config
    front + config

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,8 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "La grosse tête à Masira", "author": "Hokai" },
+            { "message": "Tu es un sorcier Harry", "author": "Harry Potter" },
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
